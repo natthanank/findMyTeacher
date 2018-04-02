cf5549452faad2e7cec134a0e323b4ff9fe2ca87
natthanank <natthanank14@gmail.com>
2018-04-02 21:15:32 +0700

    te4st

--- src/main/java/com/natthanan/findmyteacher/FindMyTeacherApplication.java
+++ src/main/java/com/natthanan/findmyteacher/FindMyTeacherApplication.java
@@ -1,10 +1,13 @@
 package com.natthanan.findmyteacher;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import com.natthanan.findmyteacher.model.Teacher;
@@ -19,6 +22,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 @LineMessageHandler
@@ -30,14 +34,16 @@ public class FindMyTeacherApplication {
     }
 
     @EventMapping
-    public List<Message> handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
 
         String inputText = event.getMessage().getText();
         // if input is course id, get teacher that take this courses
         if (inputText.matches("[-+]?\\d*\\.?\\d+")) {
-            return getTeacherFromCourseId(inputText);
+            sendPushMessage(event);
         }
-        return null;
+        // get userid for push
+        event.getSource().getUserId();
+
     }
 
     @EventMapping
@@ -64,5 +70,27 @@ public class FindMyTeacherApplication {
 
 
         return messages;
+    }
+
+    private void sendPushMessage(MessageEvent<TextMessageContent> event) {
+        final LineMessagingClient client = LineMessagingClient
+                .builder("K7AJeM33RfE2AXkEfuSAMYHxp7mEWoeMZMBD/jvwJDVwXwXTdtsbH7ZmWIu0csrlOh1Ec3smWjYnKhRaxlt2f6Aa+17Kftuw3XweTNE1IH69u8eVMy1nGGIq0pRHDzT4BsvX9YCPXfrWegeMtwzaaQdB04t89/1O/w1cDnyilFU=")
+                .build();
+
+        List<Message> messages = getTeacherFromCourseId(event.getMessage().getText());
+        BotApiResponse botApiResponse;
+        for (Message message: messages) {
+            PushMessage pushMessage = new PushMessage(
+                    event.getSource().getUserId(),
+                    message);
+            try {
+                botApiResponse = client.pushMessage(pushMessage).get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+
+
     }
 }
